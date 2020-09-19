578aef009348d1670d2d69e95e2a5c5d9e84f5e1
Jonathan Novecio <jnovecio@gmail.com>
2020-09-18 21:45:08 -0700

    perkembangan

--- Work/Controllers/WorkController.cs
+++ Work/Controllers/WorkController.cs
@@ -1,13 +1,48 @@
+using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using Work.Models;
 
 namespace Work.Controllers
 {
     public class WorkController : Controller
     {
+        private readonly List<WorkId> list = new List<WorkId>()
+        {
+            new WorkId()
+            {
+                WorkOrder = 0,
+                Facility = "Fac1",
+                EquipmentId = "P000",
+                Priority = 1,
+                Submission = new DateTime(2001, 11, 20),
+                TimeComplete = 4
+            },
+            new WorkId()
+            {
+                WorkOrder = 1,
+                Facility = "Fac2",
+                EquipmentId = "Con000",
+                Priority = 2,
+                Submission = new DateTime(2001, 11, 21),
+                TimeComplete = 5
+            },
+            new WorkId()
+            {
+                WorkOrder = 2,
+                Facility = "Fac4",
+                EquipmentId = "Sep000",
+                Priority = 4,
+                Submission = new DateTime(2001, 11, 24),
+                TimeComplete = 1
+            },
+        };
+        
+        
         [HttpGet("api/user")]
         public IActionResult Get()
         {
-            return Ok(new Models.Work{Id=1, Name = "hi"});
+            return this.Ok(list);
         }
     }
 }
--- Work/Models/Work.cs
+++ Work/Models/Work.cs
@@ -1,8 +1,14 @@
+using System;
+
 namespace Work.Models
 {
-    public class Work
+    public class WorkId
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
+        public int WorkOrder { get; set; }
+        public string Facility { get; set; }
+        public string EquipmentId { get; set; }
+        public int Priority { get; set; }
+        public int TimeComplete { get; set; }
+        public DateTime Submission { get; set; }
     }
 }
